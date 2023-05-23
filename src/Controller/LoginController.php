93c5f6373b2c91bc586739e14a35e8558c446789
Maciej Janta-Lipinski <maciekjl1993@gmail.com>
2023-05-23 18:21:05 +0200

    Fixed security

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -12,7 +12,6 @@ class LoginController extends AbstractController
     #[Route('/login', name: 'app_login')]
     public function index(AuthenticationUtils $authenticationUtils): Response
     {
-        // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
         return $this->render('login/index.html.twig', [
